9aeb32402010354851552ff63ad2938a4b6cf2d9
Matthew Bucci <buccim2@gene.com>
2016-05-12 16:35:59 -0700

    pull in a basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'build');
+var APP_DIR = path.resolve(__dirname, 'app');
+
+var config = {
+  entry: APP_DIR + '/app.js',
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel'
+      }
+    ]
+  },
+  output: {
+    path: BUILD_DIR + '/js',
+    filename: 'bundle.js'
+  },
+};
+
+module.exports = config;
