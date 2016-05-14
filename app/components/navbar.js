a1460880293fb791c9b2c3f9efc37c53660e5d8e
Matthew Bucci <buccim2@gene.com>
2016-05-14 04:26:32 -0700

    pull in topbar

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -3,9 +3,7 @@ import React from 'react';
 class Navbar extends React.Component {  
   render() {
     return (
-      <div className="navbar">
-        <h1>Navbar</h1>
-      </div>
+       <div>Topbar</div>
     );
   }
 }
