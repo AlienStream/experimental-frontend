991e6579f8f7f03fbeb547c24a1e569a8c71d6b0
Matthew Bucci <buccim2@gene.com>
2016-05-16 15:17:55 -0700

    make it more react like, don't allow youtube to change our dom

--- app/components/youtube_player.js
+++ app/components/youtube_player.js
@@ -3,37 +3,56 @@ import React from 'react';
 class YoutubePlayer extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            embeddedPlayer: '',
+            loaded: false
+        };
     }
 
     componentDidMount() {
         var currentTrack = this.props.track;
 
         if (this.trackCanBeHandled(currentTrack)) {
-            this.player = new window.YT.Player('YTplayer', {
-                width: "100%",
-                height: "300px",
-                events: {
-                    'onReady': () => {this.play(currentTrack.embeddable.url)},
-                    'onStateChange': this.onPlayerStateChange,
-                    'onError': this.onError,
-                }
+            var embedUrl = "https://youtube.com/embed/" + this.getVideoId(currentTrack.embeddable.url);
+            this.setState({
+                embeddedPlayer: "<iframe " +
+                    "id=\"YTplayer\" " +
+                    "frameborder=\"0\" " +
+                    "allowfullscreen=\"1\"  " +
+                    "style=\"width: 100%; height: 300px;\" " +
+                    "src='" + embedUrl + "'}>" +
+                "</iframe>",
+                loaded: true
             });
         }
     }
 
     render() {
-        return <div id="YTplayer"></div>;
+
+        // on first initialization grab an instance of the player
+        if (this.state.loaded == true && this.player === undefined) {
+            this.player = window.YT.get('YTplayer', {
+                events: {
+                    'onStateChange': this.onPlayerStateChange,
+                    'onError': this.onError,
+                }
+            });
+        }
+
+        return <div dangerouslySetInnerHTML={{__html: this.state.embeddedPlayer}}></div>;
     }
 
-    play(url) {
+    getVideoId(url) {
         url = url.replace("&amp;","%26");
         url = url.replace("attribution_link","");
         url = decodeURIComponent(url);
-        var video_id = url.match(/^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/);
+        return String(url.match(/^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/)[2]);
+    }
 
+    play(url) {
+        var video_id = this.getVideoId(url);
         if (video_id != null) {
-            this.player.loadVideoById(String(video_id[2]))
+            this.player.loadVideoById(video_id)
         } else {
             console.log("The youtube url is not valid.");
         }
